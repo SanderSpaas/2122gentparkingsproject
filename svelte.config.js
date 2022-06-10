69afb089d098bed4263dcfefa37bf8ea9af51282
Sander Spaas <sander.spaas@student.odisee.be>
2022-06-10 20:19:42 +0200

    moved the project

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		kit: {
-			adapter: adapter()
-		},
+		adapter: adapter(),
 		vite: {
 			server: {
 				fs: {
